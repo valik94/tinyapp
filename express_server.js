075d0d6d92cc63463ec6dae8f8b0d8da6aa33f87
valik94 <valentin.lyashenko@ryerson.ca>
2021-11-10 05:02:16 +0000

    updated URL Shortening PART 1 excercise complete - 1. created a form in urls_new.ejs to have frontend framework to work off of. 2. added longURL into the form and send data to our API using POST. 3. Routed (app.get) that render when user visits /urls/new and 4. created a route to handle POST request from our form. 5. Used body parser library to read POST request body and 6. generate random string and assign it to shortURL.

--- express_server.js
+++ express_server.js
@@ -2,24 +2,37 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString(str) {
+  let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for ( let i = 0; i < str.length; i++ ) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+}
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
+//URL Shortening (PART 1)
+//creating new route for user to GET request when visiting website/urls/new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+//handle POST request using body-parser library to make it readable
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  shortURL = generateRandomString(6);
+  console.log("new string url: ", shortURL);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 //route handler to handle get request and response, uses urls as key to access within the template
 app.get("/urls", (req, res) => {
@@ -39,19 +52,6 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
-
-// app.get("/hello", (req, res) => {
-//   const templateVars = { greeting: 'Hello World!' };
-//   res.render("hello_world", templateVars);
-// });
 
 
 app.listen(PORT, () => {
